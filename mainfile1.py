a5e1ee9f0d2aa2a173e28a00165de799f346b725
siddharth15121998 <mastersiddharthgupta@gmail.com>
2021-08-05 15:27:45 +0530

    mainfile1

---
+++ mainfile1.py
@@ -0,0 +1,9 @@
+while(True):
+    print("1. Add new account holder")
+    print("2. View all account holder")
+    print("3. Search a account holder using account number")
+    print("4. Deposit money in account")
+    print("5. Withdraw money from account")
+    print("6. Check balance in particular account")
+    print("7. Close a account")
+    print("8. Exit")
